3b25ac0e514b0c41f6418bbb8b4e690a5c6bb0de
datdevboi <cami172013@gmail.com>
2018-12-05 21:39:36 -0600

    Created test for our Stack

--- src/__tests__/stack.test.ts
+++ src/__tests__/stack.test.ts
@@ -1,5 +1,33 @@
+import { Stack } from "../stack/Stack";
+
+interface IUser {
+  name: string;
+}
+class User implements IUser {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
 describe("Stack", () => {
-  it("Push should work", () => {
-    expect(true).toBe(true);
+  it("Expect last in first out", () => {
+    const UserStack = new Stack<User>();
+    const user1 = new User("Natalie");
+    const user2 = new User("Ivy");
+    UserStack.push(user1);
+    UserStack.push(user2);
+
+    const lastUser = UserStack.pop();
+
+    if (lastUser) {
+      expect(lastUser.name).toBe("Ivy");
+    }
+
+    const newLastUser = UserStack.pop();
+
+    if (newLastUser) {
+      expect(newLastUser.name).toBe("Natalie");
+    }
   });
 });
